0b1c7785b724276e33ff69539b7ddc6b0c98638b
HYEON <hyeon970315@gmail.com>
2024-09-18 06:01:15 +0900

    2024-09-17 project - 제품선택 이벤트, 탭메뉴 이벤트, 색상버튼 자바스크립트

--- assets/js/View.js
+++ assets/js/View.js
@@ -72,12 +72,42 @@ function updateNavigationButtons() {
         document.querySelector('.swiper-button-prev-rcmd').style.display = '';
     }
 }
-
  */
+
+    /* -- -- -- -- -- -- circleColor 메서드 -- -- -- -- -- -- */
+const circleColor = selector => {
+    //const selector = document.querySelector( selectorName ); //console.log(selector);
+    if( !selector ) return;
+
+    selector.classList.add('color-btn');
+     
+    const span1 = document.createElement('span');
+    span1.classList.add('circle-color');
+
+    const span2 = document.createElement('span');
+    span2.classList.add('c-bg');
+
+    span1.appendChild(span2);
+    selector.appendChild(span1);
+};
+    /* -- -- -- clr_name 메서드 -- -- -- */
+const clr_name = clr => {
+    switch ( clr ) {
+        case "black":
+            return "블랙";
+            break;
+        case "white":
+            return "화이트";
+            break;
+        case "silver":
+            return "실버";
+            break;
+    }
+};
 
 ( async () => {
     //const params = utilHelper.getQuery();
-    const params = {id:1};
+    const params = {id:4};
 
     const curPageId = params.id;
 
@@ -91,13 +121,14 @@ function updateNavigationButtons() {
 
     try {
         response = await axios.get(`http://localhost:3001/camera/${curPageId}`);
-        console.log(response.data);
+        //console.log(response.data);
     } catch (e) {
         console.error(e);
         alert("요청 실패");
         return;
     }
-      
+     
+    const colorArr = response.data.color;
     document.querySelector(".main-img").setAttribute( "src", `assets/img/${response.data.thumbnail}` );
     
 
@@ -115,25 +146,7 @@ function updateNavigationButtons() {
     document.querySelector(".family-mlg").innerHTML = (camPrice*0.02).toLocaleString();
 
         /* -- -- 색상 -- -- */
-    if ( response.data.color ) {
-        /* 
-        const clr1 = response.data.color[0];    //console.log(clr1);
-        const clr2 = response.data.color[1];
-         */
-        const clr_name = clr => {
-            switch ( clr ) {
-                case "black":
-                    return "블랙";
-                    break;
-                case "white":
-                    return "화이트";
-                    break;
-                case "silver":
-                    return "실버";
-                    break;
-            }
-        };  //console.log(clr_name(clr1));
-        
+    if ( colorArr.length > 1 ) {         
         const div = document.createElement('div');
         div.classList.add('color-select');
         document.querySelector('.product-cont-color-select').appendChild(div);
@@ -146,7 +159,7 @@ function updateNavigationButtons() {
         ul.classList.add('circle-color-box');
 
         for ( let i=0; i<2; i++ ) {
-            let clr = response.data.color[i];
+            let clr = colorArr[i];
 
             const li = document.createElement('li');
 
@@ -182,6 +195,7 @@ function updateNavigationButtons() {
          
         document.querySelectorAll('.color-btn').forEach( (v,i) => {
             v.addEventListener( 'click', e => {
+                e.preventDefault();
                 const clickIdx = i;
     
                 document.querySelectorAll('.circle-color').forEach( (v1,i1) => {
@@ -204,67 +218,171 @@ function updateNavigationButtons() {
                 
             } );
         } );
-
-        document.querySelector('.selected-btn').addEventListener( 'click', e => {
-            //const current = e.currentTarget;
-            const ul = document.querySelector('.select-inner');
-            ul.classList.toggle('active');       
-    
-            const circleColor = selectorName => {
-                const selector = document.querySelector( selectorName ); 
-                selector.classList.add('color-btn');
-                
-                const span1 = document.createElement('span');
-                span1.classList.add('circle-color');
-        
-                const span2 = document.createElement('span');
-                span2.classList.add('c-bg');
-        
-                span1.appendChild(span2);
-                selector.appendChild(span1);
-            };
-            
-            const colorNum = response.data.color.length;
-            for ( let i=0; i<colorNum; i++ ) {
-                const li = document.createElement('li');
-                li.classList.add('inner-list');
-
-                if ( colorNum > 1 ) {
-                    const a = document.createElement('a');
-        
-                    const span1 = document.createElement('span');
-                    span1.classList.add('circle-prd-clr');
-        
-                    circleColor('.circle-prd-clr');
-        
-                    a.appendChild(span1);
-                    li.appendChild(a);
-                    ul.appendChild(li);
-                }
-                
-            } 
-        
-/* 
-            document.querySelectorAll('.inner-list').forEach( (v,i) => {
-                const a = document.createElement('a');
-    
-                const span1 = document.createElement('span');
-                span1.classList.add('circle-prd-clr');
-    
-                circleColor('.circle-prd-clr');
-    
-                a.appendChild(span1);
-    
-                v.appendChild(a);
-    
-                
-            } ); */
-            
-        } )
-        
     }
 
+        /* 제품을 선택하세요 */
+    const ulSelectInner = document.querySelector('.select-inner');
+
+    const colorNum = colorArr.length; 
+    for ( let i=0; i<colorNum; i++ ) {
+        const li = document.createElement('li');
+        li.classList.add('inner-list');
+        
+        const a = document.createElement('a');
+        
+        li.appendChild(a);
+        ulSelectInner.appendChild(li);
+    } 
+    document.querySelectorAll('.inner-list').forEach( (v,i) => {
+        let span1, span2;
+        if ( colorNum > 1 ) {
+            span1 = document.createElement('span');
+            span1.classList.add('circle-prd-clr');
+            span1.classList.add('pad');
+
+            circleColor(span1);                                        
+
+            const colorName = colorArr[i];
+            span1.classList.add( colorName ); 
+            
+            span2 = document.createElement('span');
+            span2.classList.add('submenu-txt');
+            span2.classList.add('pad');
+            span2.innerHTML = `${response.data.title} / ${clr_name(colorName)}`;
+        }
+        else {
+            span2 = document.createElement('span');
+            span2.classList.add('submenu-txt');
+            span2.classList.add('pad');
+            span2.innerHTML = `제품명: ${response.data.title}`;
+        }
+        const a = v.children[0];
+        if ( span1 ) a.appendChild(span1);
+        a.appendChild(span2);
+    } );
     
+    document.querySelector('.selected-btn').addEventListener( 'click', e => {
+        e.preventDefault();
+        ulSelectInner.classList.toggle('active');       
+    } );    
+
+        /* -- -- 제품 선택 - 선택된 제품 박스 -- -- */
+    
+    document.querySelectorAll('.inner-list').forEach( (v,i) => {
+        
+        v.addEventListener( 'click', e => {
+            e.preventDefault();
+            const current = e.currentTarget;
+            
+            const createBox = () => {
+                const div1 = document.createElement('div');
+                div1.classList.add('selected-opt');
+        
+                const div2 = document.createElement('div');
+                div2.classList.add('selected-opt-container');
+                
+                const div3 = document.createElement('div');
+                div3.classList.add('selected-prd-top');
+        
+                const p = document.createElement('p');
+                p.classList.add('prd');
+                p.innerHTML = '제품';
+        
+                const a = document.createElement('a');
+                a.classList.add('prd-delete');
+                a.innerHTML = '구매목록에서 삭제';
+        
+                div3.appendChild(p);
+                div3.appendChild(a);
+
+                const div4 = document.createElement('div');
+                div4.classList.add('selected-name');
+        
+                const circlePrdClr = current.querySelector('.circle-prd-clr');
+                const submenuTxt = current.querySelector('.submenu-txt');
+                div4.appendChild( circlePrdClr.cloneNode(true) );
+                div4.appendChild( submenuTxt.cloneNode(true) );
+                //console.log(div4);
+
+                const div5 = document.createElement('div');
+                div5.classList.add('selected-bottom');
+
+                const div6 = document.createElement('div');
+                div6.classList.add('count-container');
+
+                const btn1 = document.createElement('button');
+                btn1.classList.add('minus');
+                btn1.classList.add('cnt-box');
+                btn1.innerHTML = '-';
+
+                const input = document.createElement('input');
+                input.classList.add('cnt-box');
+                input.setAttribute( 'type', 'text' );
+                input.setAttribute( 'value', 1 );
+
+                const btn2 = document.createElement('button');
+                btn2.classList.add('plus');
+                btn2.classList.add('cnt-box');
+                btn2.innerHTML = '+';
+
+                div6.appendChild(btn1);
+                div6.appendChild(input);
+                div6.appendChild(btn2);
+
+                const div7 = document.createElement('div');
+                div7.classList.add('selected-price');
+                div7.innerHTML = `${response.data.price.toLocaleString()}원`;
+
+                div5.appendChild(div6);
+                div5.appendChild(div7);
+
+                div2.appendChild(div3);
+                div2.appendChild(div4);
+                div2.appendChild(div5);
+
+                div1.appendChild(div2);
+                //console.log(div1);
+                
+                document.querySelector('.prd-select-box').appendChild(div1);
+            };  
+ 
+            if ( document.querySelector('.selected-opt') ) {
+                document.querySelectorAll('.selected-opt').forEach( (v1,i1) => {
+                    if( v1.querySelector(`.${colorArr[i]}`) ) {
+                        //console.log(colorArr[i]);
+                        alert('이미 선택된 옵션입니다.');
+                        return;
+                    } else {
+                        createBox();
+                    }                    
+                } );
+            } else {
+                createBox();
+            }
+            
+            ulSelectInner.classList.remove('active');
+
+            //console.log(document.querySelectorAll('.prd-delete'));
+        } );
+
+        
+    } );
+/* 
+    const selectedOpt = document.querySelectorAll('.selected-opt');
+    console.log(selectedOpt);
+     */
+    
+    document.querySelectorAll('.btn-icon-container').forEach( (v,i) => {
+        const a = document.createElement('a');
+        a.classList.add('btn-icon');
+        v.appendChild(a);
+
+        //const urlName = v.classList[0];
+        const iconUrl = `../img/ico_${v.classList[0]}.svg`;
+        console.log(iconUrl);
+        //v.style.background = `url(${url}) no-repeat center`;
+        v.style.background = `url(${iconUrl}) no-repeat center`;
+    } );
 
     
     
@@ -338,31 +456,31 @@ function updateNavigationButtons() {
     } );    
     */
     
+    document.querySelectorAll('.tab-menu').forEach( (v,i) => {
+        v.addEventListener( 'click', e => {
+            const current = e.currentTarget;
+            current.classList.add('active');
+            
+            document.querySelectorAll('.tab-menu').forEach( (w,j) => {
+                if ( w !== current ) {
+                    w.classList.remove('active');
+                }
+            } );
+
+            document.querySelectorAll('.tab-zone').forEach( (v1,i1) => {
+                if ( i == i1 ) {
+                    v1.classList.add('active');
+                } else {
+                    v1.classList.remove('active');
+                }
+            } );
+        } );
+    } );
+
 } )();
 
+
  
-/* -- -- 색상 선택 -- -- */
-/* document.querySelectorAll('.color-btn').forEach( (v,i) => {
-    v.addEventListener( 'click', e => {
-        const clickIdx = i;
-
-        document.querySelectorAll('.circle-color').forEach( (v1,i1) => {
-            if ( clickIdx == i1 ) {
-                v1.classList.add('active');
-            } else {
-                v1.classList.remove('active');
-            }
-        } );
-        document.querySelectorAll('.color-name').forEach( (v2,i2) => {
-            if ( clickIdx == i2 ) {
-                v2.classList.add('active');
-            } else {
-                v2.classList.remove('active');
-            }
-        } );
-        
-    } );
-} ); */
 
 
-//const heart = document.querySelector(".heart");
+
